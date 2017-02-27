d57f02e85763fef4c5f8512f031fad568b587f14
Thomas Fitzpatrick <t_f@me.com>
2017-02-27 17:13:39 -0500

    views and controllers updated.

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -69,6 +69,6 @@ class ScoresController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def score_params
-      params.require(:score).permit(:score)
+      params.require(:score).permit(:score, :user_id)
     end
 end
