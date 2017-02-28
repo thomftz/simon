e53dd0572ffe851abe5fc68e7a9d1a59a4d24b35
katherine-hill <katherinehill1991@gmail.com>
2017-02-28 18:31:51 -0500

    fe// merging

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,15 +1,18 @@
 (function(ng) {
-  ng.module('TestDemo', ['ui.router', 'templates']).config(function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
+    ng.module('TestDemo', ['ui.router', 'templates']).config(function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
 
-    $stateProvider.state('ParentState', {
-      url: '/',
-      abstract: true,
-      template: '<ui-view></ui-view>'
-    }).state('ParentState.index', {
-      url: '',
-      controller: 'IndexController',
-      templateUrl: 'index.html'
+        $stateProvider.state('ParentState', {
+            url: '/',
+            abstract: true,
+            template: '<ui-view></ui-view>'
+        }).state('ParentState.index', {
+            url: '',
+            controller: 'IndexController',
+            templateUrl: 'index.html'
+        }).state('ParentState.gameactive', {
+            url: 'game',
+            templateUrl: 'gameactive.html'
+        });
     });
-  });
 })(angular);
