0899b229ef8621bbcf76d105d5e924ce7b5ca71c
Thomas Fitzpatrick <t_f@me.com>
2017-02-28 16:58:25 -0500

    Fixed angular templates and scss main

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,5 @@
 //= require angular-ui-router/release/angular-ui-router
 //= require angular-local-storage/dist/angular-local-storage
 //= require angular-rails-templates
+//= require_tree ../templates
 //= require_tree .
