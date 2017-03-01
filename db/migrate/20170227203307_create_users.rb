7a8634da050e50a0602ce1ac9ad34214d1e5a7ea
Thomas Fitzpatrick <t_f@me.com>
2017-03-01 13:36:58 -0500

    Google Oauth working. No styling and minimal html to display the signed in link

--- db/migrate/20170227203307_create_users.rb
+++ db/migrate/20170227203307_create_users.rb
@@ -3,9 +3,12 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password
       t.string :about
       t.string :image
+      t.string :provider
+      t.string :uid
+      t.string :oauth_token
+      t.datetime :oauth_expires_at
 
 
       t.timestamps
