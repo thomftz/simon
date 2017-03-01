9868b2518bc506d2c68e98bfa45ec9c7ad82836b
katherine-hill <katherinehill1991@gmail.com>
2017-03-01 18:36:12 -0500

    displaying full name in user profile

--- app/assets/javascripts/controllers/profile-controller.js
+++ app/assets/javascripts/controllers/profile-controller.js
@@ -3,10 +3,8 @@
     ng.module('Simon').controller('ProfileController', ['$scope', '$interpolate', function($scope, $interpolate) {
 
         if (currentUser) {
-            $scope.firstName = currentUser.name.split(' ', 1);
-            $scope.userName = $scope.firstName.value;
 
-            console.log($scope.userName);
+            $scope.userName = currentUser;
 
 
 
