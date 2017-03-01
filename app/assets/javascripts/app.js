e62a3a6c09ba8b33ad2c26bb7cbdfeebdda85e2b
katherine-hill <katherinehill1991@gmail.com>
2017-03-01 08:08:38 -0500

    resolving master conflicts

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,6 @@
 (function(ng) {
-    ng.module('TestDemo', ['ui.router', 'templates']).config(function($stateProvider, $urlRouterProvider) {
+
+    ng.module('Simon', ['ui.router', 'templates']).config(function($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/');
 
         $stateProvider.state('ParentState', {
@@ -8,8 +9,8 @@
             template: '<ui-view></ui-view>'
         }).state('ParentState.index', {
             url: '',
-            controller: 'IndexController',
-            templateUrl: 'index.html'
+            controller: 'LogInController',
+            templateUrl: 'login.html'
         }).state('ParentState.profile', {
             url: 'profile',
             // controller: 'ProfileController as profileCtrl',
@@ -18,6 +19,12 @@
             url: 'game',
             // controller: 'GameController as gameCtrl',
             templateUrl: 'game.html'
+        }).state('ParentState.leaderboard', {
+            url: 'leaderboard',
+            // controller: 'GameController as gameCtrl',
+            templateUrl: 'leaderboard.html'
         });
     });
+
+
 })(angular);
