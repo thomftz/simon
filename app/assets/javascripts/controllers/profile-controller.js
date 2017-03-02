8e117a92e75c5bd964f670c226daf80616f1c598
carter magee <cartermagee01@gmail.com>
2017-03-02 17:55:44 -0500

    ng show if user has bio

--- app/assets/javascripts/controllers/profile-controller.js
+++ app/assets/javascripts/controllers/profile-controller.js
@@ -1,6 +1,10 @@
 (function(ng, currentUser) {
 
   ng.module('Simon').controller('ProfileController', ['$scope', '$q', 'DataService', function($scope, $q, DataService) {
+    // document.querySelector('.testbtn').addEventListener('click', function() {
+    //   $scope.getBioText();
+    //   // console.log('in');
+    // });
     // console.log(currentUser.id);
     $scope.bio = null;
     $scope.userName = currentUser;
@@ -8,14 +12,28 @@
     if (currentUser) {
       $scope.signedIn = true;
     }
-    $scope.getBioText = function(text) {
-      $scope.bio = text;
+
+    $q.when(DataService.get(`http://localhost:3000/users/${currentUser.id}.json`)).then((response) => {
+      console.log(response.data[0].about);
+      $scope.bio = response.data[0].about;
+
+      console.log($scope.bio);
+
+    }).catch((error) => {
+      console.log(error);
+    });
+
+
+    $scope.patchBioText = function(text) {
       $q.when(DataService.patch(text)).then((response) => {
         console.log(response.data);
+        $scope.bio = response.data.about;
       }).catch((error) => {
         console.log(error);
       });
     };
+
+
   }]);
 
 
