7931dfd9bb7c56215bb9e88113da318bee0c0564
carter magee <cartermagee01@gmail.com>
2017-03-02 14:40:09 -0500

    profile shit

--- app/assets/javascripts/controllers/profile-controller.js
+++ app/assets/javascripts/controllers/profile-controller.js
@@ -1,6 +1,6 @@
 (function(ng, currentUser) {
-
   ng.module('Simon').controller('ProfileController', function($scope, $q, DataService) {
+    console.log(currentUser.image);
 
     $scope.userName = currentUser;
     $scope.signedIn = false;
