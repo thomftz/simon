ec7af047b837c3a269bc82c3d80f7c941677b4ba
carter magee <cartermagee01@gmail.com>
2017-03-02 07:19:10 -0500

    q and styled leaderboard

--- app/assets/javascripts/controllers/leaderboard-controller.js
+++ app/assets/javascripts/controllers/leaderboard-controller.js
@@ -4,12 +4,12 @@
     // this.points = this.users.scores;
     let btn = document.querySelector('.testbtn');
     btn.addEventListener('click', function() {});
-  });
 
-  $q.when(DataService.get("http://localhost:3000/users.json")).then((response) => {
-    this.users = response.data;
-    console.log(this.users);
-  }).catch((error) => {
-    console.log(error);
+    $q.when(DataService.get("http://localhost:3000/users.json")).then((response) => {
+      this.users = response.data;
+      console.log(response);
+    }).catch((error) => {
+      console.log(error);
+    });
   });
 })(angular);
