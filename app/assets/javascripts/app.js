6af56e1fcf2bead168acfb7e4b3012e902b64cbf
katherine-hill <katherinehill1991@gmail.com>
2017-03-03 10:30:18 -0500

    game timer start working

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,23 +1,23 @@
 (function(ng) {
-  ng.module('Simon', ['ui.router', 'templates']).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
+    ng.module('Simon', ['ui.router', 'templates']).config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
 
-    $stateProvider.state('ParentState', {
-      url: '/',
-      abstract: true,
-      template: '<ui-view></ui-view>'
-    }).state('ParentState.profile', {
-      url: '',
-      controller: 'ProfileController as profileCtrl',
-      templateUrl: 'profile.html'
-    }).state('ParentState.game', {
-      url: 'game',
-      // controller: 'GameController as gameCtrl',
-      templateUrl: 'game.html'
-    }).state('ParentState.leaderboard', {
-      url: 'leaderboard',
-      // controller: 'GameController as gameCtrl',
-      templateUrl: 'leaderboard.html'
-    });
-  }]);
+        $stateProvider.state('ParentState', {
+            url: '/',
+            abstract: true,
+            template: '<ui-view></ui-view>'
+        }).state('ParentState.profile', {
+            url: '',
+            controller: 'ProfileController as profileCtrl',
+            templateUrl: 'profile.html'
+        }).state('ParentState.game', {
+            url: 'game',
+            controller: 'GameController as gameCtrl',
+            templateUrl: 'game.html'
+        }).state('ParentState.leaderboard', {
+            url: 'leaderboard',
+            // controller: 'GameController as gameCtrl',
+            templateUrl: 'leaderboard.html'
+        });
+    }]);
 })(angular);
---
+++ app/assets/javascripts/controllers/game-controller.js
@@ -0,0 +1,36 @@
+(function(ng) {
+    ng.module('Simon').controller('GameController', ['$scope', '$q', function($scope, $q) {
+        const startTime = new Date().getTime();
+        let timer = 20;
+        let timerEnd = 0;
+        let turnDisplay = document.querySelector('.centershape');
+        let countDown = document.createElement('span');
+
+
+
+        let start = document.querySelector('.start-game');
+        start.addEventListener('click', function() {
+            $scope.startGame();
+        });
+
+        $scope.startGame = function() {
+            countDown.className = 'count-down';
+            countDown.innerHTML = `${timer}`;
+            turnDisplay.appendChild(countDown);
+
+
+
+        };
+
+        start.onclick = function() {
+            setInterval(function() {
+                timer--;
+                document.querySelector('.count-down').value = timer;
+                if (timer < 0)
+                    clearInterval(startTimer);
+                countDown.innerHTML = `${timer}`;
+            }, 1000);
+        };
+
+    }]);
+})(angular);
