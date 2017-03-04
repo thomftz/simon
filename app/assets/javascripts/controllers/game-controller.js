2d9fcc3fed0365969b4286043b9ce693cfbe2efc
carter magee <cartermagee01@gmail.com>
2017-03-04 12:19:05 -0500

    game-controller.js/game.scss - got the color to change on mousedown and mouseup on slices

--- app/assets/javascripts/controllers/game-controller.js
+++ app/assets/javascripts/controllers/game-controller.js
@@ -1,75 +1,81 @@
 (function(ng) {
-    ng.module('Simon').controller('GameController', ['$scope', '$q', function($scope, $q) {
+  ng.module('Simon').controller('GameController', ['$scope', '$q', function($scope, $q) {
 
-        let green = $('.topright').data('id');
-        let red = $('.topleft').data('id');
-        let blue = $('.bottomleft').data('id');
-        let yellow = $('.bottomright').data('id');
-        let timer = 20;
-        // let timerEnd = 0;
-        let turnDisplay = document.querySelector('.centershape');
-        let countDown = document.createElement('span');
-        let start = document.querySelector('.start-game');
-        let patternArray = [];
-        let num = 0;
-        const slices = [red, green, blue, yellow];
+    let green = $('.topright').data('id');
+    let red = $('.topleft').data('id');
+    let blue = $('.bottomleft').data('id');
+    let yellow = $('.bottomright').data('id');
+    let timer = 20;
+    // let timerEnd = 0;
+    let turnDisplay = document.querySelector('.centershape');
+    let countDown = document.createElement('span');
+    let start = document.querySelector('.start-game');
+    let patternArray = [];
+    let userArr = [];
+    let num = 0;
+    const slices = [red, green, blue, yellow];
 
-        start.onclick = function() {
-            $scope.gamePattern();
-        };
+    start.onclick = function() {
+      $scope.gamePattern();
+    };
 
-        $('.slice').on('click', function() {
-            console.log($(this).data('id'));
-        });
+    $('.slice').on('mousedown mouseup', function() {
+      console.log($(this).data('id'));
+      $(this).toggleClass('select');
+    });
+    // $('.slice').mousedown(function() {
+    //   console.log('in');
+    // });
 
-        $scope.gamePattern = function() {
-            while (patternArray.length < slices.length) {
-                let rand = slices[Math.floor(Math.random() * slices.length)];
-                patternArray.push(rand);
-            }
-            console.log(patternArray);
+    $scope.gamePattern = function() {
+      while (patternArray.length < slices.length) {
+        let rand = slices[Math.floor(Math.random() * slices.length)];
+        patternArray.push(rand);
+      }
+      console.log(patternArray);
+      setTimeout(function() {
+        $scope.lightGame();
+      }, 1000);
+    };
+
+    $scope.lightGame = function() {
+      for (let i = 0; i < patternArray.length; i++) {
+        setTimeout(function(i) {
+          setTimeout(function() {
+            console.log(patternArray[i]);
+
+            $(`.slice[data-id=${patternArray[i]}]`).toggleClass('select');
+          }, 1000);
+          $(`.slice[data-id=${patternArray[i]}]`).toggleClass('select');
+          num++;
+          console.log('nummmmmmm', num);
+          if (num == patternArray.length) {
             setTimeout(function() {
-                $scope.lightGame();
-            }, 1000);
-        };
+              $scope.startGame();
 
-        $scope.lightGame = function() {
-            for (let i = 0; i < patternArray.length; i++) {
-                setTimeout(function(i) {
-                    setTimeout(function() {
-                        console.log(patternArray[i]);
+            }, 1500);
+          }
+        }, i * 1500, i);
+      }
+    };
 
-                        $(`.slice[data-id=${patternArray[i]}]`).toggleClass('active');
-                    }, 1000);
-                    $(`.slice[data-id=${patternArray[i]}]`).toggleClass('active');
-                    num++;
-                    console.log('nummmmmmm', num);
-                    if (num == patternArray.length) {
-                        $scope.startGame();
-                    }
-                }, i * 1500, i);
-            }
+    $scope.startGame = function() {
+      console.log('in');
 
-        };
+      countDown.className = 'count-down';
+      countDown.innerHTML = `${timer}`;
+      turnDisplay.appendChild(countDown);
+      startTimer = setInterval(function() {
+        timer--;
+        $('.count-down').val(timer);
+        if (timer === 0) {
+          clearInterval(startTimer);
+          countDown.innerHTML = `you lose ${currentUser.name}`;
+        } else {
+          countDown.innerHTML = `${timer}`;
+        }
+      }, 1000);
+    };
 
-        $scope.startGame = function() {
-            console.log('in');
-            countDown.className = 'count-down';
-            countDown.innerHTML = `${timer}`;
-            turnDisplay.appendChild(countDown);
-            startTimer = setInterval(function() {
-                timer--;
-                $('.count-down').val(timer);
-                if (timer === 0) {
-                    clearInterval(startTimer);
-                    countDown.innerHTML = 'you lose trickkk';
-                } else {
-                    countDown.innerHTML = `${timer}`;
-                }
-
-            }, 1000);
-        };
-
-
-    }]);
+  }]);
 })(angular);
